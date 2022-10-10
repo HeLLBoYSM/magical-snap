a5182ce7b4db044127bef61d4b1314e2285a7014
Mihai Serban <serban_mihai_2013@yahoo.com>
2022-10-10 14:30:57 +0300

    Cards

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Cards from './components/Cards';
 
 
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Cards></Cards>
     <Footer />
   </React.StrictMode>
 );
