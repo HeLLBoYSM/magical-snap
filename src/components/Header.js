a6a345cc136b929040e151c7bd4051d60442a5a8
Mihai Serban <serban_mihai_2013@yahoo.com>
2022-10-10 14:07:32 +0300

    Header-Footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import CameraAltTwoToneIcon from '@mui/icons-material/CameraAltTwoTone';
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ['Booking', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const ResponsiveAppBar = () => {
