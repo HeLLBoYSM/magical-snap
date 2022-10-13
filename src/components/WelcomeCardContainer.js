ab1209cbee0113550c572e81e813086cb7c171ad
Mihai Serban <serban_mihai_2013@yahoo.com>
2022-10-13 14:22:37 +0300

    welcome message

---
+++ src/components/WelcomeCardContainer.js
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+
+const WelcomeCardContainer = styled(Box)(() => ({
+    display: 'flex',
+    flexWrap: 'wrap',
+    margin: 20,
+    marginBottom:50,
+    justifyContent: 'space-around',
+    alignContent:'space-around',
+}));
+
+
+
+export default WelcomeCardContainer;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Cards from './components/Cards';
-import CardsContainer from './components/CardsContainer'
+import CardsContainer from './components/CardsContainer';
+import WelcomeCard from './components/WelcomeCard';
+import WelcomeCardContainer from './components/WelcomeCardContainer';
+
 
 
 
@@ -11,6 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <WelcomeCardContainer><WelcomeCard></WelcomeCard></WelcomeCardContainer>
     <CardsContainer><Cards></Cards></CardsContainer>
     <Footer />
   </React.StrictMode>
